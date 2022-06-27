df53739a2140b8533bb25591b278391b1ee77581
zhemingjenny <108295849+zhemingjenny@users.noreply.github.com>
2022-06-27 12:54:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Favoriates')
-streamlit.text(🍲'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥤'Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥤Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
